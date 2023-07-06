c4f7755cda7e6dfd8c0a91946a106eb8e3a90ef1
Apavayan Sinha <info@apavayan.com>
2023-07-06 07:05:50 +0530

    Added API for Posts

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,65 +1,25 @@
-import Link from 'next/link'
-import dbConnect from '../lib/dbConnect'
-import Post from '../models/Post'
+import Link from "next/link";
+import dbConnect from "@/lib/dbConnect";
+import Post from "@/models/Post";
 
-const Index = ({ pets }) => (
-  <>
-    {/* Create a card for each pet */}
-    {pets.map((pet) => (
-      <div key={pet._id}>
-        <div className="card">
-          <img src={pet.image_url} />
-          <h5 className="pet-name">{pet.name}</h5>
-          <div className="main-content">
-            <p className="pet-name">{pet.name}</p>
-            <p className="owner">Owner: {pet.owner_name}</p>
-
-            {/* Extra Pet Info: Likes and Dislikes */}
-            <div className="likes info">
-              <p className="label">Likes</p>
-              <ul>
-                {pet.likes.map((data, index) => (
-                  <li key={index}>{data} </li>
-                ))}
-              </ul>
-            </div>
-            <div className="dislikes info">
-              <p className="label">Dislikes</p>
-              <ul>
-                {pet.dislikes.map((data, index) => (
-                  <li key={index}>{data} </li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="btn-container">
-              <Link href="/[id]/edit" as={`/${pet._id}/edit`} legacyBehavior>
-                <button className="btn edit">Edit</button>
-              </Link>
-              <Link href="/[id]" as={`/${pet._id}`} legacyBehavior>
-                <button className="btn view">View</button>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
-    ))}
-  </>
-)
-
-/* Retrieves pet(s) data from mongodb database */
-export async function getServerSideProps() {
-  await dbConnect()
-
-  /* find all the data in our database */
-  const result = await Post.find({})
-  const pets = result.map((doc) => {
-    const pet = doc.toObject()
-    pet._id = pet._id.toString()
-    return pet
-  })
-
-  return { props: { pets: pets } }
+export default function HomePage() {
+  return (
+    <div>index</div>
+  )
 }
 
-export default Index
+
+/* Retrieves post(s) data from mongodb database */
+export async function getServerSideProps() {
+  await dbConnect();
+
+  /* find all the data in our database */
+  const result = await Post.find({});
+  const posts = result.map((doc) => {
+    const post = doc.toObject();
+    post._id = post._id.toString();
+    return post;
+  });
+
+  return { props: { posts: posts } };
+}
